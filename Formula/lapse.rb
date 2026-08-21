4a9b53d13556df407563c3e60461d8be16e56143
achuthhadnoor <armorachyuth123@gmail.com>
2026-08-21 21:57:34 +0530

    chore(release): bump lapse formula to v0.2.0

--- Formula/lapse.rb
+++ Formula/lapse.rb
@@ -1,7 +1,7 @@
 class Lapse < Formula
   desc "Ambient timelapse screen & window recorder CLI"
   homepage "https://getlapseapp.com"
-  version "0.1.0"
+  version "0.2.0"
   license "MIT"
 
   depends_on "ffmpeg"
@@ -9,10 +9,10 @@ class Lapse < Formula
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/achuthhadnoor/homebrew-tap/releases/download/v#{version}/lapse-aarch64-apple-darwin.tar.gz"
-      sha256 "3257b78e59613ac534e0fdd9b414328fe8071f25648960d692e8060ada7455fc"
+      sha256 "bea5c9cf18dfe2726abc26cbc73c86cb9e5f180894d50bfd416544da819d30e2"
     else
       url "https://github.com/achuthhadnoor/homebrew-tap/releases/download/v#{version}/lapse-x86_64-apple-darwin.tar.gz"
-      sha256 "3257b78e59613ac534e0fdd9b414328fe8071f25648960d692e8060ada7455fc"
+      sha256 "bea5c9cf18dfe2726abc26cbc73c86cb9e5f180894d50bfd416544da819d30e2"
     end
   end
 
