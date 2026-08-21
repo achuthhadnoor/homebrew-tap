5a0437599b5320ea396f3cb97bd7a4edde6c3af3
achuthhadnoor <armorachyuth123@gmail.com>
2026-08-21 20:57:32 +0530

    feat: prompt for filename and export options before stitching, saving to Documents/Lapse

--- Formula/lapse.rb
+++ Formula/lapse.rb
@@ -9,10 +9,10 @@ class Lapse < Formula
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/achuthhadnoor/homebrew-tap/releases/download/v#{version}/lapse-aarch64-apple-darwin.tar.gz"
-      sha256 "efc09da24a2e9ddbb90e9b55ffdea556fd257be945ba3f6609ad3e4d64a8a0d1"
+      sha256 "4c2d3066fce7ffbff0d8869a9c365d606c5f3998bef0b1b4040f92dd5036d49a"
     else
       url "https://github.com/achuthhadnoor/homebrew-tap/releases/download/v#{version}/lapse-x86_64-apple-darwin.tar.gz"
-      sha256 "efc09da24a2e9ddbb90e9b55ffdea556fd257be945ba3f6609ad3e4d64a8a0d1"
+      sha256 "4c2d3066fce7ffbff0d8869a9c365d606c5f3998bef0b1b4040f92dd5036d49a"
     end
   end
 
